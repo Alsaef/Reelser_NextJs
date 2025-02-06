505545d831df2a2adc69941839507e35a987579c
Alsaef <saef.ratul@gmail.com>
2025-02-06 22:22:30 +0600

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+import { authOptions } from "@/lib/auth";
 import { DBConnection } from "@/lib/db";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
 
-export default  function Home() {
+export default async  function Home() {
+  const session = await getServerSession(authOptions);
 
+  if (!session) {
+    return <p>Access Denied. Please sign in.</p>;
+  }
   return (
   <div>
     <h2 className="text-center text-red-500">Hello world</h2>
+    <p>{session.user.email}</p>
   </div>
   );
 }
